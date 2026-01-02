7e1743684a3fcb4329d256791c7dbcb01901c17a
ShyamSundar-SF4699 <shyamsundar.d@syncfusion.com>
2026-01-02 12:03:02 +0530

    Included the LiquidGlass sample for NavigationDrawer

---
+++ NavigationDrawer/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace NavigationDrawer
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPrimaryDrawerToggled(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+        }
+    }
+}
